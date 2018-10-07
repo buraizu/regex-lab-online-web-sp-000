323698cf492d33ad63ea03658eaaa6a7b08cab35
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-07 08:13:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  
+  if phone.match?(/\d{10}/) || phone.match?(/\d{3,4}/)
+    return true
+  else
+    return false
+  end
 end
